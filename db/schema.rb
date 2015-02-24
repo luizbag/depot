8ce3ecc6eada86bf61851adb22fda569a5caf52e
Luiz Gavioli <luizbag@gmail.com>
2015-02-24 17:26:15 -0300

    Ajustando o Gemfile para versão 4.2.0

--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,12 @@
 
 ActiveRecord::Schema.define(version: 20140804171221) do
 
-  create_table "carts", force: true do |t|
+  create_table "carts", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "line_items", force: true do |t|
+  create_table "line_items", force: :cascade do |t|
     t.integer  "product_id"
     t.integer  "cart_id"
     t.datetime "created_at"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 20140804171221) do
   add_index "line_items", ["cart_id"], name: "index_line_items_on_cart_id"
   add_index "line_items", ["product_id"], name: "index_line_items_on_product_id"
 
-  create_table "products", force: true do |t|
+  create_table "products", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.string   "image_url"
