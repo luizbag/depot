01c8307f1074c7777ef5113446f8a5387295c661
Luiz Gavioli <luizbag@gmail.com>
2015-02-25 18:37:14 -0300

    Capturing and Order

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140804171221) do
+ActiveRecord::Schema.define(version: 20150225210408) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at"
@@ -24,10 +24,21 @@ ActiveRecord::Schema.define(version: 20140804171221) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "quantity",   default: 1
+    t.integer  "order_id"
   end
 
   add_index "line_items", ["cart_id"], name: "index_line_items_on_cart_id"
+  add_index "line_items", ["order_id"], name: "index_line_items_on_order_id"
   add_index "line_items", ["product_id"], name: "index_line_items_on_product_id"
+
+  create_table "orders", force: :cascade do |t|
+    t.string   "name"
+    t.text     "address"
+    t.string   "email"
+    t.string   "pay_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
